ddeb84b21bf6fe040b6e923d386786e235b7fb24
siddhiii2409 <siddhibulbule2409@gmail.com>
2026-04-06 17:52:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,43 @@ function openImage(src) {
 function closeImage() {
     document.getElementById("popup").style.display = "none";
 }
+let currentPage = 0;
+const imagesPerPage = 4;
+
+const gallery = document.querySelectorAll("#gallery img");
+
+function showPage() {
+    gallery.forEach(img => img.style.display = "none");
+
+    let start = currentPage * imagesPerPage;
+    let end = start + imagesPerPage;
+
+    for (let i = start; i < end && i < gallery.length; i++) {
+        gallery[i].style.display = "block";
+    }
+}
+
+function nextPage() {
+    if ((currentPage + 1) * imagesPerPage < gallery.length) {
+        currentPage++;
+        showPage();
+    }
+}
+
+function prevPage() {
+    if (currentPage > 0) {
+        currentPage--;
+        showPage();
+    }
+}
+
+function openImage(src) {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("popup-img").src = src;
+}
+
+function closeImage() {
+    document.getElementById("popup").style.display = "none";
+}
+
+showPage();
