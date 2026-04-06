4ea62315001978ee4f749a4beebcb5828517db5b
siddhiii2409 <siddhibulbule2409@gmail.com>
2026-04-06 17:45:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function toggleMode() {
     document.body.classList.toggle("dark");
 }
+function openImage(src) {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("popup-img").src = src;
+}
+
+function closeImage() {
+    document.getElementById("popup").style.display = "none";
+}
